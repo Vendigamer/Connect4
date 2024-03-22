cedcc4e28fccec64d75d1d985eac0a949493cd79
Vendigamer <vendi2006@gmail.hu>
2024-03-22 12:51:14 +0100

    Def

--- Connect4/Program.cs
+++ Connect4/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Xml.Linq;
+
+int TERMX = Console.WindowWidth;
+int TERMY = Console.WindowHeight;
+
+Console.WriteLine(" ██████╗ ██████╗ ███╗   ██╗███╗   ██╗███████╗ ██████╗████████╗    ███████╗ ██████╗ ██╗   ██╗██████╗ ");
+Console.WriteLine("██╔════╝██╔═══██╗████╗  ██║████╗  ██║██╔════╝██╔════╝╚══██╔══╝    ██╔════╝██╔═══██╗██║   ██║██╔══██╗");
+Console.WriteLine("██║     ██║   ██║██╔██╗ ██║██╔██╗ ██║█████╗  ██║        ██║       █████╗  ██║   ██║██║   ██║██████╔╝");
+Console.WriteLine("██║     ██║   ██║██║╚██╗██║██║╚██╗██║██╔══╝  ██║        ██║       ██╔══╝  ██║   ██║██║   ██║██╔══██╗");
+Console.WriteLine("╚██████╗╚██████╔╝██║ ╚████║██║ ╚████║███████╗╚██████╗   ██║       ██║     ╚██████╔╝╚██████╔╝██║  ██║");
+Console.WriteLine(" ╚═════╝ ╚═════╝ ╚═╝  ╚═══╝╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═╝       ╚═╝      ╚═════╝  ╚═════╝ ╚═╝  ╚═╝");
+Console.WriteLine("Nyomj ENTER-t a továbblépéshez");
+Console.ReadLine();
+
+
+
+static void show(int[,] board)
+{
+    Console.Clear();
+    string[] cells = { " ", "●", "○" };
+
+    Console.WriteLine("\n\n");
+    Console.WriteLine("  1   2   3   4   5   6   7 ");
+    for (int row = 5; row >= 0; row--)
+    {
+        Console.Write("+---+---+---+---+---+---+---+\n|");
+        for (int col = 0; col < 7; col++)
+        {
+            Console.Write(" " + cells[board[row, col]] + " |");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine("+---+---+---+---+---+---+---+\n\n\n");
+}
+
+static int[,] create_board()
+{
+    return new int[6, 7];
+}
