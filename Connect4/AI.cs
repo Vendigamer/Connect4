2e982e36817c74bc40384de51747f5954ef4ef94
DomaGamerRDG <ress.dominik.ferenc@students.jedlik.eu>
2024-04-03 21:20:56 +0200

    ai - test - failed

--- Connect4/AI.cs
+++ Connect4/AI.cs
@@ -11,21 +11,23 @@ namespace Connect4
     {
         int x = 5, y = 0;
 
-        public int firstAiTurn()
+        public void firstAiTurn()
         {
             for (int i = 0; i < 7; i++)
             {
-                if (tabla[5, i] == "X")
+                if (board[5, i] == "X")
                 {
-                    try
+                    if (i - 1 >= 0)
                     {
-                        tabla[5, i - 1] = "O";
+                        board[5, i - 1] = "O";
                         y = i - 1;
+                        return;
                     }
-                    catch
+                    else if (i + 1 < 7)
                     {
-                        tabla[5, i + 1] = "O";
+                        board[5, i + 1] = "O";
                         y = i + 1;
+                        return;
                     }
                 }
             }
@@ -35,40 +37,46 @@ namespace Connect4
         {
             if (checkHorizontal(x, y) == 3 && checkVertical(x, y) == 3)
             {
-                if (r.Next(0, 1) == 0)
+                if (r.Next(0, 2) == 0)
                 {
-                    if (tabla[x, y - 1] != "X")
+                    if (y - 1 >= 0 && board[x, y - 1] != "X")
                     {
-                        tabla[x, y - 1] == "O";
+                        board[x, y - 1] = "O";
                         y = y - 1;
                     }
-                    else
+                    else if (y + 1 < board.GetLength(1) && board[x, y + 1] != "X")
                     {
-                        tabla[x, y + 1] == "O";
+                        board[x, y + 1] = "O";
                         y = y + 1;
                     }
                 }
                 else
                 {
-                    tabla[x + 1, y] == "O";
-                    x = x + 1;
+                    if (x + 1 < board.GetLength(0) && board[x + 1, y] != "X")
+                    {
+                        board[x + 1, y] = "O";
+                        x = x + 1;
+                    }
                 }
             }
             else if (checkVertical(x, y) == 3)
             {
-                tabla[x + 1, y] == "O";
-                x = x + 1;
+                if (x + 1 < board.GetLength(0) && board[x + 1, y] != "X")
+                {
+                    board[x + 1, y] = "O";
+                    x = x + 1;
+                }
             }
             else if (checkHorizontal(x, y) == 3)
             {
-                if (tabla[x, y - 1] != "X")
+                if (y - 1 >= 0 && board[x, y - 1] != "X")
                 {
-                    tabla[x, y - 1] == "O";
+                    board[x, y - 1] = "O";
                     y = y - 1;
                 }
-                else
+                else if (y + 1 < board.GetLength(1) && board[x, y + 1] != "X")
                 {
-                    tabla[x, y + 1] == "O";
+                    board[x, y + 1] = "O";
                     y = y + 1;
                 }
             }
@@ -76,56 +84,54 @@ namespace Connect4
             {
                 for (int i = 0; i < 7; i++)
                 {
-                    if (tabla[5, i] == "X")
+                    if (board[5, i] == "X")
                     {
-                        tabla[4, i] == "O";
-                        y = i;
+                        board[4, i] = "O";
                         x = 4;
-                    }
-                }
-            }
-        }
-
-        public void checkAiTurn()
-        {
-            Random r = new Random();
-            if (checkOponentVertical(oponentX, oponentY) == true)
-            {
-                if (table[oponentX + 1, oponentY] == "*" && oponentX + 1 < table.GetLength(0))
-                {
-                    table[oponentX + 1, oponentY] = "O";
-                    x = oponentX + 1;
-                }
-                else
-                    avargeAiTurn();
-            }
-            else if (checkOponentHorizontal(oponentX, oponentY) == true)
-            {
-                for (int i = 0; i < 7; i++)
-                {
-                    if (table[oponentX, i] == "X" && table[oponentX, i + 1] == "X" && table[oponentX, i + 2] == "X" && table[oponentX, i + 3] == "*" || table[oponentX, i - 1] == "*")
-                    {
-                        if (table[oponentX, i + 3] == "*" && i + 3 < table.GetLength(1))
-                        {
-                            table[oponentX, i + 3] = "O";
-                            y = i + 3;
-                        }
-                        else if (table[oponentX, i - 1] == "*" && i - 1 < table.GetLength(1))
-                        {
-                            table[oponentX, i - 1] = "O";
-                            y = i - 1;
-                        }
-                        else
-                            avargeAiTurn();
+                        y = i;
                         break;
                     }
                 }
             }
-            else
-                avargeAiTurn();
         }
 
-        public void avargeAiTurn()
+        public void CheckAiTurn()
+        {
+            Random r = new Random();
+            if (checkOponentVertical(oponentX, oponentY))
+            {
+                if (oponentX + 1 < board.GetLength(0) && board[oponentX + 1, oponentY] == "*")
+                {
+                    board[oponentX + 1, oponentY] = "O";
+                    x = oponentX + 1;
+                    return;
+                }
+            }
+            if (checkOponentHorizontal(oponentX, oponentY))
+            {
+                for (int i = 0; i < 7; i++)
+                {
+                    if (board[oponentX, i] == "X" && i + 2 < board.GetLength(1) && board[oponentX, i + 1] == "X" && board[oponentX, i + 2] == "X")
+                    {
+                        if ((i + 3 < board.GetLength(1)) && (board[oponentX, i + 3] == "*" && (board[oponentX - 1, i + 3] == "O" || board[oponentX - 1, i + 3] == "X")))
+                        {
+                            board[oponentX, i + 3] = "O";
+                            y = i + 3;
+                            return;
+                        }
+                        else if ((i - 1 >= 0) && (board[oponentX, i - 1] == "*" && (board[oponentX - 1, i - 1] == "O" || board[oponentX - 1, i - 1] == "X")))
+                        {
+                            board[oponentX, i - 1] = "O";
+                            y = i - 1;
+                            return;
+                        }
+                    }
+                }
+            }
+            averageAiTurn();
+        }
+
+        public void averageAiTurn()
         {
 
         }
@@ -143,66 +149,79 @@ namespace Connect4
 
         public bool checkOponentHorizontal(int x, int y)
         {
-            try
+            if (x - 1 >= 0 && x + 1 < board.GetLength(0) && y >= 0 && y < board.GetLength(1))
             {
-                if (tabla[x - 1, y] == "X" && tabla[x + 1, y] == "X" || tabla[x - 2, y] == "X" && tabla[x - 1, y] == "X" || tabla[x + 2, y] == "X" && tabla[x + 1, y] == "X")
+                if ((board[x - 1, y] == "X" && board[x + 1, y] == "X") ||
+                    (x - 2 >= 0 && board[x - 2, y] == "X" && board[x - 1, y] == "X") ||
+                    (x + 2 < board.GetLength(0) && board[x + 2, y] == "X" && board[x + 1, y] == "X"))
+                {
                     return true;
-                return false;
+                }
             }
-            catch { return false; }
+            return false;
         }
 
         public bool checkOponentVertical(int x, int y)
         {
-            try
+            if (x >= 0 && x < board.GetLength(0) && y - 2 >= 0 && y < board.GetLength(1))
             {
-                if (tabla[x, y - 1] == "X" && tabla[x, y - 2] == "X")
+                if (board[x, y - 1] == "X" && board[x, y - 2] == "X")
+                {
                     return true;
-                return false;
+                }
             }
-            catch { return false; }
+            return false;
         }
 
         public int checkVertical(int x, int y)
         {
-            try
+            if (y + 3 < board.GetLength(1))
             {
-                if (tabla[x, y + 1] == "*")
+                if (board[x, y + 1] == "*" && board[x, y + 2] == "*" && board[x, y + 3] == "*")
                 {
-                    if (tabla[x, y + 2] == "*")
-                    {
-                        if (tabla[x, y + 3] == "*")
-                        {
-                            return 3;
-                        }
-                        return 2;
-                    }
+                    return 3;
+                }
+                else if (board[x, y + 1] == "*" && board[x, y + 2] == "*")
+                {
+                    return 2;
+                }
+                else if (board[x, y + 1] == "*")
+                {
                     return 1;
                 }
-                return 0;
             }
-            catch { return 0; }
+            return 0;
         }
 
         public int checkHorizontal(int x, int y)
         {
-            try
+            // Ellenőrizzük az indexek érvényességét
+            if (x - 3 >= 0 && x + 3 < board.GetLength(0) && y >= 0 && y < board.GetLength(1))
             {
-                if (tabla[x - 1, y] == "*" || tabla[x + 1, y] == "*")
+                // Ha az x-1 vagy x+1 indexek értéke "*" és az x-2 vagy x+2 indexek is "*" vagy az x-3 vagy x+3 indexek is "*"
+                if ((board[x - 1, y] == "*" && board[x - 2, y] == "*") ||
+                    (board[x + 1, y] == "*" && board[x + 2, y] == "*") ||
+                    (board[x - 1, y] == "*" && board[x - 2, y] == "*" && board[x - 3, y] == "*") ||
+                    (board[x + 1, y] == "*" && board[x + 2, y] == "*" && board[x + 3, y] == "*"))
                 {
-                    if (tabla[x - 1, y] == "*" && tabla[x - 2, y] == "*" || tabla[x + 1, y] == "*" && tabla[x + 2, y] == "*")
-                    {
-                        if (tabla[x - 1, y] == "*" && tabla[x - 2, y] == "*" && tabla[x - 3, y] == "*" || tabla[x + 1, y] == "*" && tabla[x + 2, y] == "*" && tabla[x + 3, y] == "*")
-                        {
-                            return 3;
-                        }
-                        return 2;
-                    }
+                    return 3;
+                }
+                // Ha az x-1 vagy x+1 indexek értéke "*" és az x-2 vagy x+2 indexek közül valamelyik nem "*"
+                else if ((board[x - 1, y] == "*" && board[x - 2, y] != "*") ||
+                         (board[x + 1, y] == "*" && board[x + 2, y] != "*"))
+                {
+                    return 2;
+                }
+                // Ha az x-1 vagy x+1 indexek értéke "*" és az x-2 vagy x+2 indexek közül egyik sem "*" vagy az x-3 vagy x+3 indexek értéke "*" és az x-1 vagy x+1 indexek közül valamelyik nem "*"
+                else if ((board[x - 1, y] == "*" && board[x - 2, y] != "*" && board[x + 1, y] != "*") ||
+                         (board[x + 1, y] == "*" && board[x + 2, y] != "*" && board[x - 1, y] != "*") ||
+                         (board[x - 1, y] != "*" && board[x + 1, y] == "*" && board[x + 2, y] != "*") ||
+                         (board[x + 1, y] != "*" && board[x - 1, y] == "*" && board[x - 2, y] != "*"))
+                {
                     return 1;
                 }
-                return 0;
             }
-            catch { return 0; }
+            return 0;
         }
     }
 }
--- Connect4/Program.cs
+++ Connect4/Program.cs
@@ -260,5 +260,221 @@
                 
             return restart;
         }
+
+        //int x = 5, y = 0;
+
+        //public void firstAiTurn(char[,] board)
+        //{
+        //    for (int i = 0; i < 7; i++)
+        //    {
+        //        if (board[5, i] == 'X')
+        //        {
+        //            if (i - 1 >= 0)
+        //            {
+        //                board[5, i - 1] = 'O';
+        //                //y = i - 1;
+        //                return;
+        //            }
+        //            else if (i + 1 < 7)
+        //            {
+        //                board[5, i + 1] = 'O';
+        //                //y = i + 1;
+        //                return;
+        //            }
+        //        }
+        //    }
+        //}
+
+        //public void secondAiTurn(char[,] board)
+        //{
+        //    Random r = new Random();
+        //    if (checkHorizontal(x, y, board) == 3 && checkVertical(x, y, board) == 3)
+        //    {
+        //        if (r.Next(0, 2) == 0)
+        //        {
+        //            if (y - 1 >= 0 && board[x, y - 1] != 'X')
+        //            {
+        //                board[x, y - 1] = 'O';
+        //                y = y - 1;
+        //            }
+        //            else if (y + 1 < board.GetLength(1) && board[x, y + 1] != 'X')
+        //            {
+        //                board[x, y + 1] = 'O';
+        //                y = y + 1;
+        //            }
+        //        }
+        //        else
+        //        {
+        //            if (x + 1 < board.GetLength(0) && board[x + 1, y] != 'X')
+        //            {
+        //                board[x + 1, y] = 'O';
+        //                x = x + 1;
+        //            }
+        //        }
+        //    }
+        //    else if (checkVertical(x, y, board) == 3)
+        //    {
+        //        if (x + 1 < board.GetLength(0) && board[x + 1, y] != 'X')
+        //        {
+        //            board[x + 1, y] = 'O';
+        //            x = x + 1;
+        //        }
+        //    }
+        //    else if (checkHorizontal(x, y, board) == 3)
+        //    {
+        //        if (y - 1 >= 0 && board[x, y - 1] != 'X')
+        //        {
+        //            board[x, y - 1] = 'O';
+        //            y = y - 1;
+        //        }
+        //        else if (y + 1 < board.GetLength(1) && board[x, y + 1] != 'X')
+        //        {
+        //            board[x, y + 1] = 'O';
+        //            y = y + 1;
+        //        }
+        //    }
+        //    else
+        //    {
+        //        for (int i = 0; i < 7; i++)
+        //        {
+        //            if (board[5, i] == 'X')
+        //            {
+        //                board[4, i] = 'O';
+        //                x = 4;
+        //                y = i;
+        //                break;
+        //            }
+        //        }
+        //    }
+        //}
+
+        //public void CheckAiTurn(char[,] board)
+        //{
+        //    Random r = new Random();
+        //    if (checkOponentVertical(oponentX, oponentY, board))
+        //    {
+        //        if (oponentX + 1 < board.GetLength(0) && board[oponentX + 1, oponentY] == '*')
+        //        {
+        //            board[oponentX + 1, oponentY] = 'O';
+        //            x = oponentX + 1;
+        //            return;
+        //        }
+        //    }
+        //    if (checkOponentHorizontal(oponentX, oponentY, board))
+        //    {
+        //        for (int i = 0; i < 7; i++)
+        //        {
+        //            if (board[oponentX, i] == 'X' && i + 2 < board.GetLength(1) && board[oponentX, i + 1] == 'X' && board[oponentX, i + 2] == 'X')
+        //            {
+        //                if ((i + 3 < board.GetLength(1)) && (board[oponentX, i + 3] == '*' && (board[oponentX - 1, i + 3] == 'O' || board[oponentX - 1, i + 3] == 'X')))
+        //                {
+        //                    board[oponentX, i + 3] = 'O';
+        //                    y = i + 3;
+        //                    return;
+        //                }
+        //                else if ((i - 1 >= 0) && (board[oponentX, i - 1] == '*' && (board[oponentX - 1, i - 1] == 'O' || board[oponentX - 1, i - 1] == 'X')))
+        //                {
+        //                    board[oponentX, i - 1] = 'O';
+        //                    y = i - 1;
+        //                    return;
+        //                }
+        //            }
+        //        }
+        //    }
+        //    averageAiTurn(board);
+        //}
+
+        //public void averageAiTurn(char[,] board)
+        //{
+
+        //}
+
+        //public bool checkOponentHorizontalDouble(int x, int y)
+        //{
+        //    try
+        //    {
+        //        if (tabla[x, y - 2] == "*" && tabla[x, y + 2] == "*" && tabla[x, y + 1] == "X" || tabla[x, y - 1] == "X")
+        //            return true;
+        //        return false;
+        //    }
+        //    catch { return false; }
+        //}
+
+        //public bool checkOponentHorizontal(int x, int y, char[,] board)
+        //{
+        //    if (x - 1 >= 0 && x + 1 < board.GetLength(0) && y >= 0 && y < board.GetLength(1))
+        //    {
+        //        if ((board[x - 1, y] == 'X' && board[x + 1, y] == 'X') ||
+        //            (x - 2 >= 0 && board[x - 2, y] == 'X' && board[x - 1, y] == 'X') ||
+        //            (x + 2 < board.GetLength(0) && board[x + 2, y] == 'X' && board[x + 1, y] == 'X'))
+        //        {
+        //            return true;
+        //        }
+        //    }
+        //    return false;
+        //}
+
+        //public bool checkOponentVertical(int x, int y, char[,] board)
+        //{
+        //    if (x >= 0 && x < board.GetLength(0) && y - 2 >= 0 && y < board.GetLength(1))
+        //    {
+        //        if (board[x, y - 1] == 'X' && board[x, y - 2] == 'X')
+        //        {
+        //            return true;
+        //        }
+        //    }
+        //    return false;
+        //}
+
+        //public int checkVertical(int x, int y, char[,] board)
+        //{
+        //    if (y + 3 < board.GetLength(1))
+        //    {
+        //        if (board[x, y + 1] == '*' && board[x, y + 2] == '*' && board[x, y + 3] == '*')
+        //        {
+        //            return 3;
+        //        }
+        //        else if (board[x, y + 1] == '*' && board[x, y + 2] == '*')
+        //        {
+        //            return 2;
+        //        }
+        //        else if (board[x, y + 1] == '*')
+        //        {
+        //            return 1;
+        //        }
+        //    }
+        //    return 0;
+        //}
+
+        //public int checkHorizontal(int x, int y, char[,] board)
+        //{
+        //    // Ellenőrizzük az indexek érvényességét
+        //    if (x - 3 >= 0 && x + 3 < board.GetLength(0) && y >= 0 && y < board.GetLength(1))
+        //    {
+        //        // Ha az x-1 vagy x+1 indexek értéke "*" és az x-2 vagy x+2 indexek is "*" vagy az x-3 vagy x+3 indexek is "*"
+        //        if ((board[x - 1, y] == '*' && board[x - 2, y] == '*') ||
+        //            (board[x + 1, y] == '*' && board[x + 2, y] == '*') ||
+        //            (board[x - 1, y] == '*' && board[x - 2, y] == '*' && board[x - 3, y] == '*') ||
+        //            (board[x + 1, y] == '*' && board[x + 2, y] == '*' && board[x + 3, y] == '*'))
+        //        {
+        //            return 3;
+        //        }
+        //        // Ha az x-1 vagy x+1 indexek értéke "*" és az x-2 vagy x+2 indexek közül valamelyik nem "*"
+        //        else if ((board[x - 1, y] == '*' && board[x - 2, y] != '*') ||
+        //                 (board[x + 1, y] == '*' && board[x + 2, y] != '*'))
+        //        {
+        //            return 2;
+        //        }
+        //        // Ha az x-1 vagy x+1 indexek értéke "*" és az x-2 vagy x+2 indexek közül egyik sem "*" vagy az x-3 vagy x+3 indexek értéke "*" és az x-1 vagy x+1 indexek közül valamelyik nem "*"
+        //        else if ((board[x - 1, y] == '*' && board[x - 2, y] != '*' && board[x + 1, y] != '*') ||
+        //                 (board[x + 1, y] == '*' && board[x + 2, y] != '*' && board[x - 1, y] != '*') ||
+        //                 (board[x - 1, y] != '*' && board[x + 1, y] == '*' && board[x + 2, y] != '*') ||
+        //                 (board[x + 1, y] != '*' && board[x - 1, y] == '*' && board[x - 2, y] != '*'))
+        //        {
+        //            return 1;
+        //        }
+        //    }
+        //    return 0;
+        //}
     }
 }
